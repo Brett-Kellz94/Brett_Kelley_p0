b8c852793d550c24c888aa1ed6fadf4ef140161c
Brett-Kellz94 <brett.kelley@hotmail.com>
2020-11-01 18:07:10 -0500

    New changes 11/1

--- TeaAPI/src/main/java/TeaOrder/order/customerOrderImpl.java
+++ TeaAPI/src/main/java/TeaOrder/order/customerOrderImpl.java
@@ -3,16 +3,28 @@ package TeaOrder.order;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 import TeaOrder.pojos.Customer;
 import TeaOrder.pojos.Inventory;
 import TeaOrder.pojos.Orders;
 
 public class customerOrderImpl implements customerOrder {
 	private static Scanner scan = new Scanner(System.in);
+	
+	private orderCache<Customer> customerCache = new orderCacheImpl<Customer>();
 	List<Customer> customers = new ArrayList<Customer>();
 	List<Orders> orders = new ArrayList<Orders>();
 	
+	public void setCustomerCache(orderCache<Customer> customerCache) {
+		this.customerCache = customerCache;
+	}
 
+	public customerOrderImpl(orderCache<Customer> customerCache) {
+		super();
+		this.customerCache = customerCache;
+	}
+	
+	
 	public Customer createCustomer(String name, int phoneNumber, int payment ) {
 		// prompt for customer information
 		System.out.println("Please insert customer information");
